865ea3eb2b6f606c043b7bb961c7f5addf87ebf3
Jack Mitchell <mitchell.jack@northeastern.edu>
2024-02-17 18:58:45 -0500

    Added user and organization logins with a page to choose appropriate login

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,22 @@
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/HomePage";
-import LoginPage from "./pages/LoginPage";
-import SignUp from "./pages/SignUpPage";
+import UserLoginPage from "./pages/User/UserLoginPage";
+import UserSignUpPage from "./pages/User/UserSignUpPage";
+import UserOrg from "./pages/UserOrg";
+import OrgLoginPage from "./pages/Organization/OrgLoginPage";
+import OrgSignUpPage from "./pages/Organization/OrgSignUpPage";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/signup" element={<SignUp />} />
+        <Route path="/user-login" element={<UserLoginPage />} />
+        <Route path="/user-signup" element={<UserSignUpPage />} /> 
+        <Route path="/organization-login" element={<OrgLoginPage />} />
+        <Route path="/organization-signup" element={<OrgSignUpPage />} />
+        <Route path="/user-or-org" element={<UserOrg />} />
       </Routes>
     </>
   );
