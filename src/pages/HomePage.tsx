757703742427e928deae3c31469f59c476488430
Jack Mitchell <mitchell.jack@northeastern.edu>
2024-02-18 15:50:42 -0500

    Small change

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -37,20 +37,26 @@ const HomePage = () => {
     <div className="px-10 py-20">
       <Navbar />
       <Filters></Filters>
-      <div className="grid grid-cols-4 gap-8">
-        {listings.map((listing, index) => (
-          <Listing
-            key={index}
-            orgName={listing.name}
-            orgNumber={listing.contact}
-            orgLoc={listing.address}
-            orgNeeds={listing.tags.needs}
-            time={listing.created_at}
-            uid={listing.listingId}
-            user_id={listing.user_id}
-          />
-        ))}
-      </div>
+      {listings.length > 0 ? (
+        <div className="grid grid-cols-4 gap-8">
+          {listings.map((listing, index) => (
+            <Listing
+              key={index}
+              orgName={listing.name}
+              orgNumber={listing.contact}
+              orgLoc={listing.address}
+              orgNeeds={listing.tags.needs}
+              time={listing.created_at}
+              uid={listing.listingId}
+              user_id={listing.user_id}
+            />
+          ))}
+        </div>
+      ) : (
+        <div className="text-center p-5 mt-5 bg-gray-100 rounded-lg shadow-md">
+          <p className="text-lg text-gray-800">No postings are currently listed.</p>
+        </div>
+      )}
     </div>
     <AddBtn />
     </>
